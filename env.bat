fa42ce1e206f37d96b4cac29f4e10bb7c8a9f4ed
JunYangLin <sheep6411@gmail.com>
2019-11-04 20:59:44 +0800

    Day 058 commit

--- env.bat
+++ env.bat
@@ -1,5 +1,9 @@
 cd /D E:\100Day-ML-Marathon
-call C:\ProgramData\Anaconda3\Scripts\activate.bat
+call C:\ProgramData\Miniconda3\Scripts\activate.bat
 call conda activate ml
 call jupyter notebook
 cmd /k
+
+
+
+
